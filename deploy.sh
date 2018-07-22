8064df44673694cfe362b03e90e6b19104335c43
sufuf3 <sufuf3@gmail.com>
2018-07-22 22:10:30 +0800

    rebuilding site Sun Jul 22 22:10:30 CST 2018

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
-hugo --theme=min_night # if using a theme, replace by `hugo -t <yourtheme>`
+hugo --theme=hyde-hyde # if using a theme, replace by `hugo -t <yourtheme>`
 # Go To Public folder
 cd public
 # Add changes to git.
