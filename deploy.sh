5f093a40c03a50319c4ffe8909699eaa08fb8e84
Samina Fu <sufuf3@gmail.com>
2022-04-27 20:26:55 +0800

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
-hugo --theme=hyde-hyde # if using a theme, replace by `hugo -t <yourtheme>`
+hugo --theme=personal-web # if using a theme, replace by `hugo -t <yourtheme>`
 # Go To Public folder
 cd public
 # Add changes to git.
